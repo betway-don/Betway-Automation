f019454bdf01a76cc2a862fa5a4dc10fdc5c4a1a
Atharva Deshmukh <AD115408@zender.local>
2025-07-29 17:30:13 +0530

    Add file structure

---
+++ playwright.GH.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src/regions/GH/tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+    ['html', { outputFolder: 'src/regions/GH/reports/html-report' }],
+    ['allure-playwright', { outputFolder: 'src/regions/GH/reports/allure-results' }]
+  ],
+  use: {
+    // baseURL: 'http://gh.example.com',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
